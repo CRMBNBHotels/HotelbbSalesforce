4ef05d7885a10bd231564ea96134cc458a409469
cynthiageagea <cynthia.geagea@eit-mena.com>
2020-10-28 11:21:52 +0200

    NeoDev to NeoDev1 and more

---
+++ .sfdx/typings/lwc/LC03_WalletHistory_WidthResource.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/LC03_WalletHistory_WidthResource" {
+    var LC03_WalletHistory_WidthResource: string;
+    export default LC03_WalletHistory_WidthResource;
+}
---
+++ .sfdx/typings/lwc/paymentMethodeImage.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/paymentMethodeImage" {
+    var paymentMethodeImage: string;
+    export default paymentMethodeImage;
+}
