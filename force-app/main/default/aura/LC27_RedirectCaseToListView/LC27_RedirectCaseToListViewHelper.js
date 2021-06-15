25380e3b79d0a833ccf3fbb0c024b66f551c662a
amiramungroo <a.mungroo@kanelservices.com>
2021-06-15 10:41:06 +0400

    Changes for CRMSS-1202/ MS-7561: Suppression des Cases

---
+++ force-app/main/default/aura/LC27_RedirectCaseToListView/LC27_RedirectCaseToListViewHelper.js
@@ -0,0 +1,9 @@
+({ invoke : function(component, event, helper) {
+    
+   var redirectURL = component.get("v.redirectURL");
+   var redirect = $A.get("e.force:navigateToURL");
+   redirect.setParams({
+      "url": redirectURL
+   });
+   redirect.fire();
+}})
